0784d5a761f0375fdb51114340420513681437b5
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-18 00:37:10 +0200

    axios

--- listakorisnika/src/components/App.js
+++ listakorisnika/src/components/App.js
@@ -5,12 +5,18 @@ import axios from 'axios';
 
 const App = () => {
     const [userData, setUserData] = useState([]);
-  
+    const [reset, setReset]=useState(false);
+
 useEffect(()=>{
-    fetch('./database.json').then((response)=>response.json()).then((data)=>{
-        setUserData(data);
-    })
-})
+    // fetch('./database.json').then((response)=>response.json()).then((data)=>{
+    //     setUserData(data);
+    // })
+    const getData=async()=>{
+    const res=await axios.get('./database.json');
+    setUserData(res.data);
+    }
+    getData();
+}, [reset])
 
     return (
         <div>
@@ -18,7 +24,7 @@ useEffect(()=>{
                 <em className="text-success">Trenutno imamo {userData.length} korisnika u bazi</em>
                 <List userData={userData} />
                 <button onClick={()=>{setUserData([])}} className="btn btn-danger m-2">Isprazni listu</button>
-                <button onClick={()=>{setUserData([])}} className="btn btn-success m-2">Popuni listu</button>
+                <button onClick={()=>{setReset([true])}} className="btn btn-success m-2">Popuni listu</button>
 
             </div>
         </div>
